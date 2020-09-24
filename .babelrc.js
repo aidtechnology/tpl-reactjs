17c17e1e172104ee7ebfe039a5c63ae9e8f88faa
Ben Cessa <ben@pixative.com>
2020-09-24 11:47:49 -0400

    Babel: class properties plugin

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,6 @@
 const presets = ["@babel/preset-env", "@babel/preset-react"];
 const plugins = [
+  ["@babel/plugin-proposal-class-properties", {}, "class-properties-support"],
   [
     "babel-plugin-module-resolver",
     {
